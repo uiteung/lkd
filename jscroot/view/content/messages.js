4937ad0c3849716589cf056e9df30a9b872cf5cf
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-05 06:32:33 +0700

    css

--- jscroot/view/content/messages.js
+++ jscroot/view/content/messages.js
@@ -1,13 +1,13 @@
-import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.2/croot.js";
+import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.4/croot.js";
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
 
 
-export function main(){
-    setInner("biggreet","Halo Ngadimin");
+export async function main(){
+    await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
     Swal.fire({
         icon: "success",
         title: "Anda tidak memiliki pesan baru",
         showConfirmButton: false,
         timer: 1500
-      });
+    });
 }
