8adb5d0155950ff1f8c31df4196de603f9410962
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-10 13:24:15 +0700

    addCSSIn

--- jscroot/view/content/explore.js
+++ jscroot/view/content/explore.js
@@ -1,8 +1,9 @@
 import {map} from '../../controller/peta.js';
-import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {id} from '../../url/config.js';
+import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 
 
 export async function main(){
-    await addCSS("assets/css/styles.css");
+    await addCSSIn("assets/css/styles.css",id.content);
     map.updateSize();
 }
