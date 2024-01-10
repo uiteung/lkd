de106c3aeefdb1016970bb9c19aa3c6bfbe5d872
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-10 16:32:37 +0700

    cuba

--- jscroot/view/content/explore.js
+++ jscroot/view/content/explore.js
@@ -1,9 +1,34 @@
-import {map} from '../../controller/peta.js';
+import Map from 'https://cdn.skypack.dev/ol/Map.js';
+import View from 'https://cdn.skypack.dev/ol/View.js';
+import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
+import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
+import {fromLonLat} from 'https://cdn.skypack.dev/ol/proj.js';
+
+
 import {id} from '../../url/config.js';
 import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 
 
+const attributions = '<a href="https://petapedia.github.io/" target="_blank">&copy; PetaPedia Indonesia</a> ';
+const place = [107.13563336552649,-6.8165156551551505];
+const basemap = new TileLayer({
+    source: new OSM({attributions: attributions,}),
+  });
+  
+const defaultstartmap = new View({
+center: fromLonLat(place),
+zoom: 9,
+});
+
+let map;
+
 export async function main(){
     await addCSSIn("assets/css/styles.css",id.content);
-    map.updateSize();
+    map = new Map({
+        target: 'map',
+        layers: [
+            basemap
+        ],
+        view: defaultstartmap,
+      });
 }
