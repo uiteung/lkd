01d915ac6f005348446f6761d77be1d8787c8e0d
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-10 13:14:16 +0700

    add css

--- jscroot/view/content/explore.js
+++ jscroot/view/content/explore.js
@@ -1,12 +1,7 @@
 import {map} from '../../controller/peta.js';
 
 
-export function main(){
-    var viewHeight = $(window).height();
-    var header = $("div[data-role='header']:visible:visible");
-    var navbar = $("div[data-role='navbar']:visible:visible");
-    var content = $("div[data-role='content']:visible:visible");
-    var contentHeight = viewHeight - header.outerHeight() - navbar.outerHeight();
-    content.height(contentHeight);
+export async function main(){
+    await addCSS("assets/css/styles.css");
     map.updateSize();
 }
