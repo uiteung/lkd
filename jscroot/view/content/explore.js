b4917def39004445ae7ec3dc93e3eea4cba83de8
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-10 13:12:14 +0700

    update

--- jscroot/view/content/explore.js
+++ jscroot/view/content/explore.js
@@ -2,5 +2,11 @@ import {map} from '../../controller/peta.js';
 
 
 export function main(){
-    const map = map;
+    var viewHeight = $(window).height();
+    var header = $("div[data-role='header']:visible:visible");
+    var navbar = $("div[data-role='navbar']:visible:visible");
+    var content = $("div[data-role='content']:visible:visible");
+    var contentHeight = viewHeight - header.outerHeight() - navbar.outerHeight();
+    content.height(contentHeight);
+    map.updateSize();
 }
